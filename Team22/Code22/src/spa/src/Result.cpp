1e9b5cd1b93be89d302ff6d91f73474657a110e9
Jai Lulla <jaylulla9@gmail.com>
2023-02-02 23:06:57 +0800

    Add Basic Functionality for Result Class

--- Team22/Code22/src/spa/src/Result.cpp
+++ Team22/Code22/src/spa/src/Result.cpp
@@ -1,3 +1,27 @@
 //
 // Created by Jai Lulla  on 2/2/23.
 //
+
+#include <iostream>
+
+#include "Result.h"
+
+using namespace std;
+
+inline Result::Result(vector<string> result) {
+    this->result = result;
+}
+
+string Result::toString() {
+    string resultString = "";
+
+    for (auto item: this->result) {
+        resultString += (item + ", ");
+    }
+
+    return resultString;
+}
+
+vector<string> Result::getResult() {
+    return this->result;
+}
--- Team22/Code22/src/spa/src/Result.h
+++ Team22/Code22/src/spa/src/Result.h
@@ -2,7 +2,25 @@
 // Created by Jai Lulla  on 2/2/23.
 //
 
+#pragma once
+
 #ifndef SPA_RESULT_H
 #define SPA_RESULT_H
+
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class Result {
+    vector<string> result;
+
+public:
+    inline explicit Result(vector<string> result);
+
+    string toString();
+    vector<string> getResult();
+};
 
 #endif //SPA_RESULT_H
