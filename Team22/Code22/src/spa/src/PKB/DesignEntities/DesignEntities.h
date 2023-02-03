96246db74756ee07a45b0038b4ae4fe064882f14
Jai Lulla <jaylulla9@gmail.com>
2023-02-03 20:03:31 +0800

    Delete Renamed or Unwanted Files

--- Team22/Code22/src/spa/src/PKB/DesignEntities/DesignEntities.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Jai Lulla  on 2/2/23.
-//
-
-#ifndef SPA_DESIGNENTITIES_H
-#define SPA_DESIGNENTITIES_H
-
-#endif //SPA_DESIGNENTITIES_H
--- Team22/Code22/src/spa/src/PKB/DesignEntities/DesignEntityStorage.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Jai Lulla  on 3/2/23.
-//
-
-#include "DesignEntityStorage.h"
