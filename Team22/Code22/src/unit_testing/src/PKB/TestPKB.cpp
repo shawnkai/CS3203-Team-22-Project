1255d1c9b003b528d14f5d9957c1a107de654bba
Jai Lulla <jaylulla9@gmail.com>
2023-02-03 23:25:25 +0800

    Update Test case name

--- Team22/Code22/src/unit_testing/src/PKB/TestPKB.cpp
+++ Team22/Code22/src/unit_testing/src/PKB/TestPKB.cpp
@@ -2,12 +2,14 @@
 // Created by Jai Lulla  on 3/2/23.
 //
 
+#include <iostream>
+
 #include "catch.hpp"
 #include "PKB/PKB.h"
 
 using namespace std;
 
-TEST_CASE("1") {
+TEST_CASE("Trial PKB Test") {
     PKB pkbTest = PKB();
     pkbTest.addDesignEntity("variable", make_tuple("a", "1"));
 
