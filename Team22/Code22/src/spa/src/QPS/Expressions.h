206ed6ee982a1bcce0d1a26df84f38088960c32f
Tanishq4331 <62829987+Tanishq4331@users.noreply.github.com>
2023-02-03 18:08:02 +0800

    "Add Modifies, Select, and Uses Expressions header file"

--- Team22/Code22/src/spa/src/QPS/Expressions.h
+++ Team22/Code22/src/spa/src/QPS/Expressions.h
@@ -11,20 +11,84 @@
 #include "QPS/Entities.h"
 #include "Result.h"
 
+using namespace std;
+
 class Expression {
     virtual Result evaluate() = 0;
+
+    protected:
+        vector<DesignEntity> entities;
+
+    public:
+        explicit Expression();
 };
 
 
 class SelectExpression : Expression {
 private:
-    DesignEntity entity;
+    vector<Expression> conditions;
 
 public:
-    SelectExpression(DesignEntity entity);
+    explicit SelectExpression(vector<Expression> conditions);
 
     Result evaluate() override;
 
 };
+
+
+//Modifies Expression Classes
+class ModifiesExpression : Expression {
+
+public:
+    explicit ModifiesExpression(NamedEntity target);
+};
+
+class ModifiesSExpression : ModifiesExpression {
+private: 
+    StmtEntity modifier;
+
+public:
+    explicit ModifiesSExpression(StmtEntity modifier);
+
+    Result evaluate() override;
+};
+
+class ModifiesPExpression : ModifiesExpression {
+private:
+    NamedEntity modifier;
+
+public:
+    explicit ModifiesPExpression(NamedEntity modifier);
+
+    Result evaluate() override;
+};
+
+
+//Uses expression classes
+class UsesExpression : Expression {
+public:
+    explicit UsesExpression(DesignEntity target);
+};
+
+class UsesSExpression : UsesExpression {
+private:
+    StmtEntity user;
+
+public:
+    explicit UsesSExpression(StmtEntity user);
+
+    Result evaluate() override;
+};
+
+class UsesPExpression : UsesExpression {
+private:
+    NamedEntity user;
+
+public:
+    explicit UsesPExpression(NamedEntity user);
+
+    Result evaluate() override;
+};
+
 
 #endif //SPA_EXPRESSIONS_H
