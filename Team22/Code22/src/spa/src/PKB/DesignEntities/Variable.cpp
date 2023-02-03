fb0c80f99b8daea04cc742aea21ad1085ef86dab
Jai Lulla <jaylulla9@gmail.com>
2023-02-03 19:56:19 +0800

    Add Variable Design Entity with Method Definitions
    BUILDING: YES

---
+++ Team22/Code22/src/spa/src/PKB/DesignEntities/Variable.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Jai Lulla  on 3/2/23.
+//
+
+#include <iostream>
+
+#include "Variable.h"
+#include "DesignEntitiesDatabase/DesignEntityDatabase.h"
+#include "DesignEntitiesDatabase/DesignEntitiesDatabaseFactory.h"
+
+using namespace std;
+
+Variable::Variable(tuple<string, string> entityDetails) {
+    this->nameOfEntity = get<0>(entityDetails);
+    (this->occurrenceOfEntity).push_back(get<1>(entityDetails));
+}
+
+void Variable::addAdditionalOccurrence(string occurrence) {
+    (this->occurrenceOfEntity).push_back(occurrence);
+}
+
+void Variable::addToDatabase() {
+    // call storage/database factory and add, let storage check if there,
+    // and update or add
+    DesignEntityDatabase database = DesignEntitiesDatabaseFactory::getEntityDatabase(*this);
+    database.addToDatabase(*this);
+}
+
+string Variable::getNameOfEntity() {
+    return this->nameOfEntity;
+}
+
+vector<string> Variable::getOccurrenceOfEntity() {
+    return this->occurrenceOfEntity;
+}
+
+string Variable::getTypeOfEntity() {
+    return "VARIABLE";
+}
---
+++ Team22/Code22/src/spa/src/PKB/DesignEntities/Variable.h
@@ -0,0 +1,25 @@
+//
+// Created by Jai Lulla  on 3/2/23.
+//
+
+#ifndef SPA_VARIABLE_H
+#define SPA_VARIABLE_H
+
+#include <iostream>
+
+#include "DesignEntity.h"
+
+using namespace std;
+
+class Variable: public DesignEntity {
+public:
+    explicit Variable(tuple<string, string> entityDetails);
+
+    void addAdditionalOccurrence(string occurrence) override;
+    void addToDatabase() override;
+    string getNameOfEntity() override;
+    vector<string> getOccurrenceOfEntity() override;
+    string getTypeOfEntity() override;
+};
+
+#endif //SPA_VARIABLE_H
