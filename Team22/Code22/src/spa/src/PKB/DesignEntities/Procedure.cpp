ce8c224667fbeafc61bdea1b55797e7c12909fd8
Jai Lulla <jaylulla9@gmail.com>
2023-02-04 17:30:22 +0800

    Implement new DesignEntity: Procedure

---
+++ Team22/Code22/src/spa/src/PKB/DesignEntities/Procedure.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Jai Lulla  on 4/2/23.
+//
+
+#include <iostream>
+
+#include "Procedure.h"
+
+using namespace std;
+
+Procedure::Procedure(tuple<string, string> entityDetails) {
+    this->nameOfEntity = get<0>(entityDetails);
+    (this->occurrenceOfEntity).push_back(get<1>(entityDetails));
+}
+
+string Procedure::getTypeOfEntity() {
+    return "PROCEDURE";
+}
