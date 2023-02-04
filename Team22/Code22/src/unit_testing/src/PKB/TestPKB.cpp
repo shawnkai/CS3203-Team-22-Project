d1140ce4d0e3e0906fb8b2388cd89a4a658c259f
Jai Lulla <jaylulla9@gmail.com>
2023-02-04 14:49:34 +0800

    Add some test cases to test PKB functionality
    Passing:
    All when ran individually, YES
    If ran in sequence, NO
    
    Bugs To Fix:
    1. Look at interdependence among variables in Test cases
    2. Might be some re-initialisation issue

--- Team22/Code22/src/unit_testing/src/PKB/TestPKB.cpp
+++ Team22/Code22/src/unit_testing/src/PKB/TestPKB.cpp
@@ -10,9 +10,52 @@
 using namespace std;
 
 TEST_CASE("Trial PKB Test") {
-    PKB pkbTest = PKB();
-    pkbTest.addDesignEntity("variable", make_tuple("a", "1"));
+    SECTION("") {
+        PKB pkbTest = PKB();
+        pkbTest.addDesignEntity("variable", make_tuple("a", "1"));
+        Result pkbResult = pkbTest.getDesignEntity("VARIABLE", "a");
+
+        Result expectedResult("VARIABLE", "a", vector<string> {"1"});
 
 //    REQUIRE(pkbTest.addDesignEntity("variable", make_tuple("a", "1")))
-    REQUIRE(1==1);
+//    REQUIRE(1==1);
+        REQUIRE(pkbResult.areEqual(expectedResult));
+    }
+}
+
+// Need to section below, as some code is being carried forward unintentionally
+TEST_CASE("PKB Test 1: Same Type, Same Variable Name, Same Occurrence") {
+    SECTION("") {
+        PKB pkbTest1 = PKB();
+        pkbTest1.addDesignEntity("variable", make_tuple("a", "1"));
+        Result pkbResult1 = pkbTest1.getDesignEntity("VARIABLE", "a");
+
+        Result expectedResult1("VARIABLE", "a", vector<string>{"1"});
+
+        REQUIRE(pkbResult1.areEqual(expectedResult1));
+    }
+}
+
+TEST_CASE("PKB Test 2: Same Type, Different Variable Name, Same Occurrence") {
+    SECTION("") {
+        PKB pkbTest2 = PKB();
+        pkbTest2.addDesignEntity("variable", make_tuple("b", "1"));
+        Result pkbResult2 = pkbTest2.getDesignEntity("VARIABLE", "a");
+
+        Result expectedResult2("VARIABLE", "a", vector<string>{"1"});
+
+        REQUIRE(!pkbResult2.areEqual(expectedResult2));
+    }
+}
+
+TEST_CASE("PKB Test 3: Same Type, Different Variable Name, Different Occurrence") {
+    SECTION("") {
+        PKB pkbTest3 = PKB();
+        pkbTest3.addDesignEntity("variable", make_tuple("b", "2"));
+        Result pkbResult3 = pkbTest3.getDesignEntity("VARIABLE", "a");
+
+        Result expectedResult3("VARIABLE", "a", vector<string>{"1"});
+
+        REQUIRE(!pkbResult3.areEqual(expectedResult3));
+    }
 }
