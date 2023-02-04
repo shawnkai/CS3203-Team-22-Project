ee96b4fa66b6466a7e2746381f10802726bfebfb
Jai Lulla <jaylulla9@gmail.com>
2023-02-04 18:06:52 +0800

    Add Extra Test Cases and Fix Interdependence Bug
    1. Add Test Cases to Check Procedure and
    Variable Entity Database are working fine.
    2. Add Test Cases to Check the Expected Result
    Matches the Query Result
    
    BUILDING: YES
    TEST CASES: PASSING

--- Team22/Code22/src/unit_testing/src/PKB/TestPKB.cpp
+++ Team22/Code22/src/unit_testing/src/PKB/TestPKB.cpp
@@ -12,11 +12,18 @@ using namespace std;
 TEST_CASE("Trial PKB Test") {
     SECTION("") {
         PKB pkbTest = PKB();
-        pkbTest.addDesignEntity("variable", make_tuple("a", "1"));
+        pkbTest.addDesignEntity("VARIABLE", make_tuple("a", "1"));
+//        pkbTest.addDesignEntity("PROCEDURE", make_tuple("a", "2"));
+
         Result pkbResult = pkbTest.getDesignEntity("VARIABLE", "a");
+
+        cout << pkbResult.toString() << endl;
 
         Result expectedResult("VARIABLE", "a", vector<string> {"1"});
 
+        cout << expectedResult.toString() << endl;
+
+        cout << (pkbResult.areEqual(expectedResult) ? "Yes" : "No") << endl;
 //    REQUIRE(pkbTest.addDesignEntity("variable", make_tuple("a", "1")))
 //    REQUIRE(1==1);
         REQUIRE(pkbResult.areEqual(expectedResult));
@@ -24,38 +31,98 @@ TEST_CASE("Trial PKB Test") {
 }
 
 // Need to section below, as some code is being carried forward unintentionally
-TEST_CASE("PKB Test 1: Same Type, Same Variable Name, Same Occurrence") {
+TEST_CASE("PKB Test 1: Same Type, Same Entity Name, Same Occurrence") {
     SECTION("") {
         PKB pkbTest1 = PKB();
-        pkbTest1.addDesignEntity("variable", make_tuple("a", "1"));
-        Result pkbResult1 = pkbTest1.getDesignEntity("VARIABLE", "a");
+        pkbTest1.addDesignEntity("VARIABLE", make_tuple("a1", "1"));
+        Result pkbResult1 = pkbTest1.getDesignEntity("VARIABLE", "a1");
 
-        Result expectedResult1("VARIABLE", "a", vector<string>{"1"});
+        Result expectedResult1("VARIABLE", "a1", vector<string>{"1"});
 
         REQUIRE(pkbResult1.areEqual(expectedResult1));
     }
 }
 
-TEST_CASE("PKB Test 2: Same Type, Different Variable Name, Same Occurrence") {
+TEST_CASE("PKB Test 2: Same Type, Different Entity Name, Same Occurrence") {
     SECTION("") {
         PKB pkbTest2 = PKB();
-        pkbTest2.addDesignEntity("variable", make_tuple("b", "1"));
-        Result pkbResult2 = pkbTest2.getDesignEntity("VARIABLE", "a");
+        pkbTest2.addDesignEntity("VARIABLE", make_tuple("b2", "1"));
+        Result pkbResult2 = pkbTest2.getDesignEntity("VARIABLE", "b2");
 
-        Result expectedResult2("VARIABLE", "a", vector<string>{"1"});
+        Result unexpectedResult2("VARIABLE", "a2", vector<string>{"1"});
 
-        REQUIRE(!pkbResult2.areEqual(expectedResult2));
+        REQUIRE(!pkbResult2.areEqual(unexpectedResult2));
     }
 }
 
-TEST_CASE("PKB Test 3: Same Type, Different Variable Name, Different Occurrence") {
+TEST_CASE("PKB Test 3: Same Type, Different Entity Name, Different Occurrence") {
     SECTION("") {
         PKB pkbTest3 = PKB();
-        pkbTest3.addDesignEntity("variable", make_tuple("b", "2"));
-        Result pkbResult3 = pkbTest3.getDesignEntity("VARIABLE", "a");
+        pkbTest3.addDesignEntity("VARIABLE", make_tuple("b3", "2"));
+        Result pkbResult3 = pkbTest3.getDesignEntity("VARIABLE", "b3");
 
-        Result expectedResult3("VARIABLE", "a", vector<string>{"1"});
+        Result unexpectedResult3("VARIABLE", "a3", vector<string>{"1"});
 
-        REQUIRE(!pkbResult3.areEqual(expectedResult3));
+        REQUIRE(!pkbResult3.areEqual(unexpectedResult3));
+    }
+}
+
+TEST_CASE("PKB Test 4: Different Type, Same Entity Name, Same Occurrence") {
+    SECTION("") {
+        PKB pkbTest4 = PKB();
+        pkbTest4.addDesignEntity("PROCEDURE", make_tuple("a4", "1"));
+        Result pkbResult4 = pkbTest4.getDesignEntity("PROCEDURE", "a4");
+
+        Result unexpectedResult4("VARIABLE", "a4", vector<string>{"1"});
+
+        REQUIRE(!pkbResult4.areEqual(unexpectedResult4));
+    }
+}
+
+TEST_CASE("PKB Test 5: Different Type, Different Entity Name, Same Occurrence") {
+    SECTION("") {
+        PKB pkbTest5 = PKB();
+        pkbTest5.addDesignEntity("PROCEDURE", make_tuple("a5", "1"));
+        Result pkbResult5 = pkbTest5.getDesignEntity("PROCEDURE", "a5");
+
+        Result unexpectedResult5("VARIABLE", "b5", vector<string>{"1"});
+
+        REQUIRE(!pkbResult5.areEqual(unexpectedResult5));
+    }
+}
+
+TEST_CASE("PKB Test 6: Different Type, Different Entity Name, Different Occurrence") {
+    SECTION("") {
+        PKB pkbTest6 = PKB();
+        pkbTest6.addDesignEntity("PROCEDURE", make_tuple("a6", "1"));
+        Result pkbResult6 = pkbTest6.getDesignEntity("PROCEDURE", "a6");
+
+        Result unexpectedResult6("VARIABLE", "b6", vector<string>{"2"});
+
+        REQUIRE(!pkbResult6.areEqual(unexpectedResult6));
+    }
+}
+
+TEST_CASE("PKB Test 7: Same Type, Same Entity Name, Different Occurrence") {
+    SECTION("") {
+        PKB pkbTest7 = PKB();
+        pkbTest7.addDesignEntity("VARIABLE", make_tuple("a7", "2"));
+        Result pkbResult7 = pkbTest7.getDesignEntity("VARIABLE", "a7");
+
+        Result unexpectedResult7("VARIABLE", "a7", vector<string>{"1"});
+
+        REQUIRE(!pkbResult7.areEqual(unexpectedResult7));
+    }
+}
+
+TEST_CASE("PKB Test 8: Different Type, Same Entity Name, Different Occurrence") {
+    SECTION("") {
+        PKB pkbTest8 = PKB();
+        pkbTest8.addDesignEntity("VARIABLE", make_tuple("a8", "2"));
+        Result pkbResult8 = pkbTest8.getDesignEntity("VARIABLE", "a8");
+
+        Result unexpectedResult8("PROCEDURE", "a8", vector<string>{"1"});
+
+        REQUIRE(!pkbResult8.areEqual(unexpectedResult8));
     }
 }
