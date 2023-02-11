67f2b71aa87dc515efcc9ad9e348fa56b80bab4e
Jai Lulla <jaylulla9@gmail.com>
2023-02-11 12:20:57 +0800

    Add Comments

--- Team22/Code22/src/spa/src/PKB/DesignEntities/DesignEntitiesDatabase/DesignEntityDatabase.cpp
+++ Team22/Code22/src/spa/src/PKB/DesignEntities/DesignEntitiesDatabase/DesignEntityDatabase.cpp
@@ -8,6 +8,11 @@
 
 using namespace std;
 
+/**
+ * Adds the given Design Entity to the appropriate database.
+ *
+ * @param designEntityToBeStored The Design Entity "Reference" Object to be stored.
+ */
 void DesignEntityDatabase::addToDatabase(DesignEntity* designEntityToBeStored) {
     // First find the variable if it exists in db, if not, add.
     // If it does exist, then just retrieve and add to the vector<string> occurrence list
@@ -19,10 +24,22 @@ void DesignEntityDatabase::addToDatabase(DesignEntity* designEntityToBeStored) {
     }
 }
 
+/**
+ * Returns a boolean suggesting whether the given
+ * Design Entity is already present in the database or not.
+ *
+ * @param designEntityToBeStored The Design Entity that is to be stored, and to be checked for.
+ * @return A boolean, true, if the Entity already exists in the database.
+ */
 bool DesignEntityDatabase::isPresentInDatabase(DesignEntity* designEntityToBeStored) {
     return this->isPresentInDatabase(designEntityToBeStored->getNameOfEntity());
 }
 
+/**
+ * Updates an existing Entity if already present in the database.
+ *
+ * @param designEntityToBeStored The Design Entity that is to be stored and/or updated.
+ */
 void DesignEntityDatabase::updateEntityInDatabase(DesignEntity* designEntityToBeStored) {
     // insert new set of value
 //    (this->database)[designEntityToBeStored.getNameOfEntity()];
@@ -36,6 +53,13 @@ void DesignEntityDatabase::updateEntityInDatabase(DesignEntity* designEntityToBe
 //    toBeUpdated.push_back(designEntityToBeStored.getOccurrenceOfEntity()[0]);
 }
 
+/**
+ * Returns a boolean suggesting whether the given
+ * Design Entity is already present in the database or not.
+ *
+ * @param entityName The Design Entity Name  that is to be checked for.
+ * @return A boolean, true, if the Entity already exists in the database.
+ */
 bool DesignEntityDatabase::isPresentInDatabase(string entityName) {
     if ((this->database).find(entityName) == (this->database).end()) {
         return false;
@@ -43,6 +67,14 @@ bool DesignEntityDatabase::isPresentInDatabase(string entityName) {
     return true;
 }
 
+/**
+ * Returns a Result consisting of the Entity type queried for, given
+ * by the name. If no Entity by that name is present, a Result is still
+ * returned, but it consists of all "None" values.
+ *
+ * @param entityName The name of the Entity to be extracted.
+ * @return A Result object, consisting of the Entity details, given by the Entity name.
+ */
 Result DesignEntityDatabase::getFromDatabase(string entityName) {
     if (this->isPresentInDatabase(entityName)) {
         auto iterator = (this->database).find(entityName);
@@ -61,6 +93,12 @@ Result DesignEntityDatabase::getFromDatabase(string entityName) {
     return Result("none", "none", none);
 }
 
+/**
+ * Returns a vector consisting of all Entities of the given type
+ * present in the database.
+ *
+ * @return A vector<Result> containing all the Entities of that Entity Type.
+ */
 vector<Result> DesignEntityDatabase::getAllFromDatabase() {
     vector<Result> resultVector;
 
