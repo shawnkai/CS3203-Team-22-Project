1ae83c1cb5ab6d843c6e7cf936330a67b947a957
Diwu-Yi <59159857+Diwu-Yi@users.noreply.github.com>
2023-02-19 02:17:31 +0800

    Rename test resources and test files

--- Team22/Code22/src/unit_testing/src/SP/Parser/TestParser.cpp
+++ Team22/Code22/src/unit_testing/src/SP/Parser/TestParser.cpp
@@ -5,3 +5,64 @@
 #include "SP/Parser/Parser.h"
 #include "catch.hpp"
 using namespace std;
+
+TEST_CASE("TestCase1_ParseBasicSimpleSource_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase2_ParseWhileStmtSource_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase3_ParseIfStmtSource_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase4_ParseReadPrintStmtSource_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase5_ParseAssignStmtSource_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase6_ParseIfWhileCombinedMix1_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase7_ParseIfWhileCombinedMix2_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase8_ParseIfWhileCombinedMix3_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase9_ParseDeepNestingSource_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase10_ParseComplexConditionalStmt_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase11_ParseComplexConditionalStmtVariation2_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase12_ParseGrandSimpleSource_ShouldSuccess") {
+
+}
+
+TEST_CASE("TestCase13_ParseIllegalToken_ShouldThrowException") {
+
+}
+
+TEST_CASE("TestCase14_ParseIllegalGrammar_ShouldThrowException") {
+
+}
+
+TEST_CASE("TestCase15_ParseTanglingTokenOutsideProcedure_ShouldSuccess") {
+
+}
+
