274ea435c7817c0594258a0f605eab2195cbec7c
Jai Lulla <jaylulla9@gmail.com>
2023-02-18 21:45:09 +0800

    Add Comments and Refactor PKB.cpp File

--- Team22/Code22/src/spa/src/PKB/PKB.cpp
+++ Team22/Code22/src/spa/src/PKB/PKB.cpp
@@ -92,23 +92,53 @@ vector<Result> PKB::getAllDesignEntity(string entityType) {
     return queryResult;
 }
 
+/**
+ * This method allows to add an Assignment Pattern to the Program Knowledge Base.
+ *
+ * @param leftHandVariableName The name of the variable that is being assigned to.
+ * @param prefixExpression The expression on the right hand side, which has been converted into its prefix form.
+ * @param patternLineNumber The line number at which this assignment pattern can be observed.
+ */
 void PKB::addAssignPattern(string leftHandVariableName, string prefixExpression, string patternLineNumber) {
     AssignPattern* assignPattern = AssignPatternFactory::createAssignPattern(leftHandVariableName, prefixExpression,
                                                                              patternLineNumber);
     assignPattern->addToDatabase();
 }
 
+/**
+ * Returns a string containing the Right Hand Prefix Expression, for the given left hand variable name
+ * and line number, else returns an empty string, if any pattern stored for the given combinations
+ * cannot be found.
+ *
+ * @param leftHandVariableName The variable, which has been assigned, to be looked for.
+ * @param patternLineNumber The line number at which this assignment took place.
+ * @return A string containing the Right Hand Prefix Expression or an empty string.
+ */
 string
 PKB::getRightHandExpressionOfAVariableOnAParticularLineNumber(string leftHandVariableName, string patternLineNumber) {
     AssignPatternDatabase* assignPatternDatabase = AssignPatternDatabaseFactory::getAssignPatternDatabase();
-    return assignPatternDatabase->getRightHandExpressionOfAVariableOnAParticularLineNumberFromDatabase(leftHandVariableName, patternLineNumber);
+    return assignPatternDatabase
+    ->getRightHandExpressionOfAVariableOnAParticularLineNumberFromDatabase(leftHandVariableName, patternLineNumber);
 }
 
+/**
+ * Returns an unordered map containing all the Right Hand Prefix Expression associated to a given
+ * variable, that would have occurred on the left hand side.
+ *
+ * @param leftHandVariableName The variable, which has been assigned, to be looked for.
+ * @return An unordered map containing all the Right Hand Prefix Expressions associated to the given variable.
+ */
 unordered_map<string, string> PKB::getAllRightHandExpressionsOfAVariable(string leftHandVariableName) {
     AssignPatternDatabase* assignPatternDatabase = AssignPatternDatabaseFactory::getAssignPatternDatabase();
     return assignPatternDatabase->getAllRightHandExpressionsOfAVariableFromDatabase(leftHandVariableName);
 }
 
+/**
+ * Returns a vector of AssignPattern pointer objects, that contain all the patterns derivable from
+ * the assignment statements. This is particularly useful, when wildcards come into play.
+ *
+ * @return A vector of AssignPattern pointer objects, or an empty vector, if nothing is stored.
+ */
 vector<AssignPattern*> PKB::getAllRightHandExpressions() {
     AssignPatternDatabase* assignPatternDatabase = AssignPatternDatabaseFactory::getAssignPatternDatabase();
     return assignPatternDatabase->getAllRightHandExpressionsFromDatabase();
