689242c0597e7051286e956f3f63d079a2710e1d
Jai Lulla <jaylulla9@gmail.com>
2023-02-19 20:57:58 +0800

    Add Comments to Various Files

--- Team22/Code22/src/spa/src/PKB/Pattern/AssignPattern/AssignPatternDatabaseFactory.cpp
+++ Team22/Code22/src/spa/src/PKB/Pattern/AssignPattern/AssignPatternDatabaseFactory.cpp
@@ -8,10 +8,18 @@
 
 using namespace std;
 
+/**
+ * Returns the database, that stores all the Assignment Patterns.
+ *
+ * @return A AssignPatternDatabase pointer, that points to the Assignment Patterns database.
+ */
 AssignPatternDatabase *AssignPatternDatabaseFactory::getAssignPatternDatabase() {
     return assignPatternDatabase;
 }
 
+/*
+ * Clears the Assignment Patterns database.
+ */
 void AssignPatternDatabaseFactory::clearDatabase() {
     delete assignPatternDatabase;
 
