d5ffe2850ec5a71c46b83ad2addd346be3e15658
Diwu-Yi <59159857+Diwu-Yi@users.noreply.github.com>
2023-02-19 19:19:47 +0800

    Code clean

--- Team22/Code22/src/integration_testing/src/TestComponentsIntegration.cpp
+++ Team22/Code22/src/integration_testing/src/TestComponentsIntegration.cpp
@@ -1,13 +1,25 @@
-#include "SP/Parser/TNode.h"
+#include "SP/SPDriver.h"
+
 #include "catch.hpp"
 using namespace std;
 void require(bool b) {
     REQUIRE(b);
 }
 
-TEST_CASE("TestCase1_") {
+TEST_CASE("TestCase1_StandardExampleSIMPLESource_ShouldSuccess") {
+    SPDriver driver;
+    std::string inputFilePath;
+#if __APPLE__
+    inputFilePath = "../../../tests/Sample_source2.txt";
+#endif
+    driver.parseSimpleProgram(inputFilePath);
 
-    TNode T;
-    require(1 == 1);
+    PKB standardExampleSIMPLESourceChecker = PKB();
+    Result pkbResult = standardExampleSIMPLESourceChecker.getDesignEntity("ASSIGNMENT", "x");
+    cout << pkbResult.toString() << endl;
+    Result expectedResult("ASSIGNMENT", "x", vector<string> {"1", "5", "7", "8", "9"});
+    REQUIRE(pkbResult.areEqual(expectedResult));
+
+    //Result pkbResult2 = standardExampleSIMPLESourceChecker.getDesignEntity("CONSTANT", "z");
 }
 
