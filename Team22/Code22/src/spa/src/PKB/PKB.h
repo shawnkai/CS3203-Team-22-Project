44255e272c72ab91a014f8de2dfead581c20e8bf
Jai Lulla <jaylulla9@gmail.com>
2023-03-17 18:01:52 +0800

    Add Overloaded APIs For Design Abstractions
    - addDesignAbstraction()
    - getDesignAbstraction()
    
    These are not meant for MODIFIES and USES Design Abstraction. If
    invoked with parameters including "USES" or "MODIFIES", an exception
    would be thrown. InvalidAPICallException would be thrown.

--- Team22/Code22/src/spa/src/PKB/PKB.h
+++ Team22/Code22/src/spa/src/PKB/PKB.h
@@ -20,23 +20,30 @@ class PKB {
     void clearControlFlowGraphDatabase();
 
 public:
+    // APIs Related To Design Entities
     void addDesignEntity(string designEntity, tuple<string, string> entityDetails);
     Result getDesignEntity(string entityType, string entityName);
     vector<Result> getAllDesignEntity(string entityType);
     int getNumberOfDesignEntity(string entityType);
 
+    // APIs Related to Design Abstractions
     void addDesignAbstraction(string designAbstraction, tuple<string, string, string> abstractionDetails);
     Result getDesignAbstraction(string abstractionType, tuple<string, string> query);
+    // Overloaded APIs, for all except modifies and uses
+    void addDesignAbstraction(string designAbstraction, tuple<string, string> abstractionDetails);
+    Result getDesignAbstraction(string abstractionType, string query);
     vector<Result> getAllDesignAbstractions(string designAbstractionType, string entityTypeBeingAbstracted);
-    unordered_map<string, unordered_set<string>> getAllVariablesCapturedByDesignAbstraction(string designAbstractionType,
-                                                                             string entityTypeBeingAbstracted);
+    unordered_map<string, unordered_set<string>> getAllVariablesCapturedByDesignAbstraction(
+            string designAbstractionType, string entityTypeBeingAbstracted);
 
+    // APIs Related to Assign Pattern
     void addAssignPattern(string leftHandVariableName, string prefixExpression, string patternLineNumber);
     string getRightHandExpressionOfAVariableOnAParticularLineNumber(string leftHandVariableName,
                                                                     string patternLineNumber);
     unordered_map<string, string> getAllRightHandExpressionsOfAVariable(string leftHandVariableName);
     vector<AssignPattern*> getAllRightHandExpressions();
 
+    // APIs Related to Control Flow Graph
     void addControlFlowGraph(string procedureName, vector<int> topologicallySortedElements,
                              map<int, vector<int>> blockToStatementNumbers, map<int, int> statementNumberToBlock,
                              map<int, vector<int>> blockToBlock, unordered_set<int> blocksWithBackPointers);
@@ -46,5 +53,6 @@ public:
     map<int, vector<int>> getBlockToBlockDatabase(string procedureName);
     unordered_set<int> getBlocksWithBackPointersDatabase(string procedureName);
 
+    // API For Clearing All Databases
     void clearAllDatabases();
 };
