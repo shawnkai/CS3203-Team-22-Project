c46c34b9ca1e4de81716acdb17c4cb1dbe974aa3
Jai Lulla <jaylulla9@gmail.com>
2023-03-18 21:47:52 +0800

    Add Test Cases For Testing New APIs

--- Team22/Code22/src/unit_testing/src/PKB/TestDesignAbstractions/TestUsesDesignAbstraction/TestPrintUsesDesignAbstraction.cpp
+++ Team22/Code22/src/unit_testing/src/PKB/TestDesignAbstractions/TestUsesDesignAbstraction/TestPrintUsesDesignAbstraction.cpp
@@ -7,10 +7,12 @@
 #include "catch.hpp"
 #include "PKB/PKB.h"
 
+#include "PKB/Exceptions/InvalidAPICallException.cpp"
+
 using namespace std;
 
 TEST_CASE("Test 1: Creation of PrintUses Design Abstraction") {
-    SECTION("") {
+    SECTION("Using API With A Tuple of Size 3") {
         PKB pkbTest = PKB();
         pkbTest.addDesignAbstraction("USES", make_tuple("PRINT", "up1", "1"));
         Result pkbResult = pkbTest.getDesignAbstraction("USES", make_pair("PRINT", "up1"));
@@ -19,15 +21,43 @@ TEST_CASE("Test 1: Creation of PrintUses Design Abstraction") {
 
         REQUIRE(pkbResult.areEqual(expectedResult));
     }
+
+    SECTION("Using API With A Tuple of Size 2") {
+        bool throwsException = false;
+        PKB pkbTest = PKB();
+
+        try {
+            pkbTest.addDesignAbstraction("USES", make_tuple("up1", "1"));
+        } catch (InvalidAPICallException& e) {
+            throwsException = true;
+        }
+
+        REQUIRE(throwsException);
+    }
 }
 
 TEST_CASE("Test 2: Retrieval of an existent PrintUses Design Abstraction") {
-    SECTION("") {
+    SECTION("Using API With A Tuple of Size 2") {
         PKB pkbTest = PKB();
         pkbTest.addDesignAbstraction("USES", make_tuple("PRINT", "up2", "1"));
         Result pkbResult = pkbTest.getDesignAbstraction("USES", make_pair("PRINT", "up2"));
 
         REQUIRE(pkbResult.toString() == "USES:PRINT: up2: 1, ");
+    }
+
+    SECTION("Using API Without Tuple") {
+        bool throwsException = false;
+        PKB pkbTest = PKB();
+
+        pkbTest.addDesignAbstraction("USES", make_tuple("PRINT", "up2", "1"));
+
+        try {
+            Result result = pkbTest.getDesignAbstraction("USES", "up2");
+        } catch (InvalidAPICallException& e) {
+            throwsException = true;
+        }
+
+        REQUIRE(throwsException);
     }
 }
 
