1b94d97aed300e7cc90ccf9be6a44453d1f73fe7
Jai Lulla <jaylulla9@gmail.com>
2023-03-31 01:03:45 +0800

    Adapt Test Cases For InverseCalls Design Abstraction

--- Team22/Code22/src/unit_testing/src/PKB/TestDesignAbstractions/TestInvserseCallsDesignAbstraction.cpp
+++ Team22/Code22/src/unit_testing/src/PKB/TestDesignAbstractions/TestInvserseCallsDesignAbstraction.cpp
@@ -5,25 +5,25 @@
 #include <iostream>
 
 #include "catch.hpp"
-#include "PKB/PKB.h"
+#include "PKB/Interfaces/DesignAbstractionsController.h"
 
 using namespace std;
 
 TEST_CASE("Test 1: Creation of InverseCalls Design Abstraction") {
     SECTION("Using API With A Tuple of Size 3") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a1", "a2"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a1", "a2"));
 
-        Result result = pkbTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a1"));
+        Result result = designAbstractionsControllerTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a1"));
 
         REQUIRE(((result.getQueryEntityName() == "a1") && (result.getQueryResult().size() != 0)));
     }
 
     SECTION("Using API With A Tuple of Size 2") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("a1", "a2"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("a1", "a2"));
 
-        Result result = pkbTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a1"));
+        Result result = designAbstractionsControllerTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a1"));
 
         REQUIRE(((result.getQueryEntityName() == "a1") && (result.getQueryResult().size() != 0)));
     }
@@ -31,19 +31,19 @@ TEST_CASE("Test 1: Creation of InverseCalls Design Abstraction") {
 
 TEST_CASE("Test 2: Retrieval of InverseCalls Design Abstraction") {
     SECTION("Using API With A Tuple of Size 2") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a3", "a4"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a3", "a4"));
 
-        Result result = pkbTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a3"));
+        Result result = designAbstractionsControllerTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a3"));
 
         REQUIRE(result.toString() == "INVERSECALLS: a3: a4, ");
     }
 
     SECTION("Using API Without Tuple") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a3", "a4"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a3", "a4"));
 
-        Result result = pkbTest.getDesignAbstraction("INVERSECALLS", "a3");
+        Result result = designAbstractionsControllerTest.getDesignAbstraction("INVERSECALLS", "a3");
 
         REQUIRE(result.toString() == "INVERSECALLS: a3: a4, ");
     }
@@ -51,10 +51,10 @@ TEST_CASE("Test 2: Retrieval of InverseCalls Design Abstraction") {
 
 TEST_CASE("Test 3: Retrieval of a non-existent InverseCalls Design Abstraction") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a5", "a6"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a5", "a6"));
 
-        Result result = pkbTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a0"));
+        Result result = designAbstractionsControllerTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a0"));
 
         REQUIRE(result.toString() == "none: none: none, ");
     }
@@ -62,11 +62,11 @@ TEST_CASE("Test 3: Retrieval of a non-existent InverseCalls Design Abstraction")
 
 TEST_CASE("Test 4: Retrieval of a InverseCalls Design Abstraction When Multiple InverseCalls Design Abstractions Are Stored") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a7", "a8"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a9", "a10"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a7", "a8"));
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a9", "a10"));
 
-        Result result = pkbTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a7"));
+        Result result = designAbstractionsControllerTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a7"));
 
         REQUIRE(result.getQueryResult()[0] == "a8");
     }
@@ -74,64 +74,47 @@ TEST_CASE("Test 4: Retrieval of a InverseCalls Design Abstraction When Multiple 
 
 TEST_CASE("Test 5: Retrieval of an InverseCalls Design Abstraction When Multiple Same InverseCalls Design Abstractions Are Stored Including Duplicated Values") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a11", "a13"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a11", "a13"));
 
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a11", "a13"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a12", "a13"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a11", "a14"));
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a11", "a13"));
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a12", "a13"));
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a11", "a14"));
 
-        Result pkbResult = pkbTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a11"));
+        Result DesignAbstractionsControllerResult = designAbstractionsControllerTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a11"));
 
-        REQUIRE(pkbResult.toString() == "INVERSECALLS: a11: a13, a14, ");
+        REQUIRE(DesignAbstractionsControllerResult.toString() == "INVERSECALLS: a11: a13, a14, ");
     }
 }
 
-TEST_CASE("Test 6: Populate The InverseCalls Database And Call Clear All Database using the clearAllDatabases() API") {
+TEST_CASE("Test 6: Retrieval of All InverseCalls Design Abstractions") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a15", "a16"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a15", "a16"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a15", "a17"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a15", "a18"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a19", "a20"));
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a21", "a22"));
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a23", "a24"));
 
-        Result pkbResultBeforeClearing = pkbTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a15"));
-
-        pkbTest.clearAllDatabases();
-
-        Result pkbResultAfterClearing = pkbTest.getDesignAbstraction("INVERSECALLS", make_pair("_", "a15"));
-
-        REQUIRE(((pkbResultBeforeClearing.toString() == "INVERSECALLS: a15: a16, a17, a18, ")
-                 && (pkbResultAfterClearing.toString() == "none: none: none, ")));
-    }
-}
-
-TEST_CASE("Test 7: Retrieval of All InverseCalls Design Abstractions") {
-    SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a19", "a20"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a21", "a22"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a23", "a24"));
-
-        vector<Result> result = pkbTest.getAllDesignAbstractions("INVERSECALLS", "_");
+        vector<Result> result = designAbstractionsControllerTest.getAllDesignAbstractions("INVERSECALLS", "_");
 
         REQUIRE(result.size() == 3);
     }
 }
 
-TEST_CASE("Test 8: Retrieval of Variables Captured By InverseCalls Design Abstractions") {
+TEST_CASE("Test 7: Retrieval of Variables Captured By InverseCalls Design Abstractions") {
     SECTION("") {
-        PKB pkbTest = PKB();
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
 
-        pkbTest.clearAllDatabases();
-
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a25", "a26"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a27", "a28"));
-        pkbTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a29", "a30"));
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a25", "a26"));
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a27", "a28"));
+        designAbstractionsControllerTest.addDesignAbstraction("INVERSECALLS", make_tuple("_", "a29", "a30"));
 
         unordered_map<string, unordered_set<string>> result =
-                pkbTest.getAllVariablesCapturedByDesignAbstraction("INVERSECALLS", "_");
+                designAbstractionsControllerTest.getAllVariablesCapturedByDesignAbstraction("INVERSECALLS", "_");
 
-        REQUIRE(result.size() == 3);
+        REQUIRE(
+                ((result.find("a26") != result.end()) &&
+                 (result.find("a28") != result.end()) &&
+                 (result.find("a30") != result.end()))
+        );
     }
 }
