157591e7db6693f121f9773c18d99f237bf679f8
Jai Lulla <jaylulla9@gmail.com>
2023-03-30 22:55:38 +0800

    Add PatternsController
    - Part of transition to ISP in PKB

---
+++ Team22/Code22/src/spa/src/PKB/Interfaces/PatternsController.h
@@ -0,0 +1,29 @@
+//
+// Created by Jai Lulla  on 30/3/23.
+//
+
+#pragma once
+
+#ifndef SPA_PATTERNSCONTROLLER_H
+#define SPA_PATTERNSCONTROLLER_H
+
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class PatternsController {
+public:
+    void addAssignPattern(string leftHandVariableName, string prefixExpression, string patternLineNumber);
+    string getRightHandExpressionOfAVariableOnAParticularLineNumber(string leftHandVariableName,
+                                                                    string patternLineNumber);
+    unordered_map<string, string> getAllRightHandExpressionsOfAVariable(string leftHandVariableName);
+    unordered_map<string, unordered_map<string, string>> getAllRightHandExpressions();
+
+    void addPattern(string patternType, string lineNumber, string variableName);
+    bool isVariableUsedInPattern(string patternType, string lineNumber, string variableName);
+    unordered_set<string> getAllVariablesUsedInPattern(string patternType, string lineNumber);
+};
+
+#endif //SPA_PATTERNSCONTROLLER_H
