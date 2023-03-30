5fbdca0087031d2a50daf9311da07c585cdecab2
Jai Lulla <jaylulla9@gmail.com>
2023-03-31 01:39:01 +0800

    Adapt Test Cases For Modifies Assignment Design Abstraction

--- Team22/Code22/src/unit_testing/src/PKB/TestDesignAbstractions/TestModifiesDesignAbstraction/TestAssignmentModifiesDesignAbstraction.cpp
+++ Team22/Code22/src/unit_testing/src/PKB/TestDesignAbstractions/TestModifiesDesignAbstraction/TestAssignmentModifiesDesignAbstraction.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 #include "catch.hpp"
-#include "PKB/PKB.h"
+#include "PKB/Interfaces/DesignAbstractionsController.h"
 
 #include "PKB/Exceptions/InvalidAPICallException.cpp"
 
@@ -13,9 +13,9 @@ using namespace std;
 
 TEST_CASE("Test 1: Creation of AssignmentModifies Design Abstraction") {
     SECTION("Using API With A Tuple of Size 3") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma1", "1"));
-        Result pkbResult = pkbTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma1"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma1", "1"));
+        Result pkbResult = designAbstractionsControllerTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma1"));
 
         Result expectedResult("MODIFIES:ASSIGNMENT", "ma1", vector<string>{"1"});
 
@@ -24,10 +24,10 @@ TEST_CASE("Test 1: Creation of AssignmentModifies Design Abstraction") {
 
     SECTION("Using API With A Tuple of Size 2") {
         bool throwsException = false;
-        PKB pkbTest = PKB();
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
 
         try {
-            pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ma1", "1"));
+            designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ma1", "1"));
         } catch (InvalidAPICallException& e) {
             throwsException = true;
         }
@@ -38,21 +38,21 @@ TEST_CASE("Test 1: Creation of AssignmentModifies Design Abstraction") {
 
 TEST_CASE("Test 2: Retrieval of an existent AssignmentModifies Design Abstraction") {
     SECTION("Using API With A Tuple of Size 2") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma2", "1"));
-        Result pkbResult = pkbTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma2"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma2", "1"));
+        Result pkbResult = designAbstractionsControllerTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma2"));
 
         REQUIRE(pkbResult.toString() == "MODIFIES:ASSIGNMENT: ma2: 1, ");
     }
 
     SECTION("Using API Without Tuple") {
         bool throwsException = false;
-        PKB pkbTest = PKB();
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
 
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma2", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma2", "1"));
 
         try {
-            Result result = pkbTest.getDesignAbstraction("MODIFIES", "ma2");
+            Result result = designAbstractionsControllerTest.getDesignAbstraction("MODIFIES", "ma2");
         } catch (InvalidAPICallException& e) {
             throwsException = true;
         }
@@ -63,9 +63,9 @@ TEST_CASE("Test 2: Retrieval of an existent AssignmentModifies Design Abstractio
 
 TEST_CASE("Test 3: Retrieval of a non-existent AssignmentModifies Design Abstraction") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma3", "1"));
-        Result pkbResult = pkbTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma0"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma3", "1"));
+        Result pkbResult = designAbstractionsControllerTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma0"));
 
         REQUIRE(pkbResult.toString() == "none: none: none, ");
     }
@@ -73,10 +73,10 @@ TEST_CASE("Test 3: Retrieval of a non-existent AssignmentModifies Design Abstrac
 
 TEST_CASE("Test 4: Retrieval of an AssignmentModifies Design Abstraction When Multiple AssignmentModifies Design Abstractions Are Stored") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma4", "1"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma5", "1"));
-        Result pkbResult = pkbTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma4"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma4", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma5", "1"));
+        Result pkbResult = designAbstractionsControllerTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma4"));
 
         REQUIRE(pkbResult.toString() == "MODIFIES:ASSIGNMENT: ma4: 1, ");
     }
@@ -84,17 +84,17 @@ TEST_CASE("Test 4: Retrieval of an AssignmentModifies Design Abstraction When Mu
 
 TEST_CASE("Test 5: Retrieval of an AssignmentModifies Design Abstraction When Multiple Different Modifies Design Abstractions Are Stored") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma6", "1"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma6", "1"));
 
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("PROCEDURECALL", "ma6", "1"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("PROCEDURE", "ma6", "1"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("READ", "ma6", "1"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("STATEMENT", "ma6", "1"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("WHILE", "ma6", "1"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("IF", "ma6", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("PROCEDURECALL", "ma6", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("PROCEDURE", "ma6", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("READ", "ma6", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("STATEMENT", "ma6", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("WHILE", "ma6", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("IF", "ma6", "1"));
 
-        Result pkbResult = pkbTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma6"));
+        Result pkbResult = designAbstractionsControllerTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma6"));
 
         REQUIRE(pkbResult.toString() == "MODIFIES:ASSIGNMENT: ma6: 1, ");
     }
@@ -102,65 +102,48 @@ TEST_CASE("Test 5: Retrieval of an AssignmentModifies Design Abstraction When Mu
 
 TEST_CASE("Test 6: Retrieval of an AssignmentModifies Design Abstraction When Multiple Same Modifies Design Abstractions Are Stored Including Duplicated Values") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma7", "1"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma7", "1"));
 
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma7", "1"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma7", "2"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("READ", "ma8", "1"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma8", "3"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma7", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma7", "2"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("READ", "ma8", "1"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma8", "3"));
 
-        Result pkbResult = pkbTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma7"));
+        Result pkbResult = designAbstractionsControllerTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma7"));
 
         REQUIRE(pkbResult.toString() == "MODIFIES:ASSIGNMENT: ma7: 1, 2, ");
     }
 }
 
-TEST_CASE("Test 7: Populate The AssignmentModifies Database And Call Clear All Database using the clearAllDatabases() API") {
+TEST_CASE("Test 7: Retrieval of All AssignmentModifies Design Abstractions") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma9", "ma10"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma9", "ma10"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma9", "ma11"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma9", "ma12"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma13", "ma14"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma15", "ma16"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma17", "ma18"));
 
-        Result pkbResultBeforeClearing = pkbTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma9"));
+        vector<Result> result = designAbstractionsControllerTest.getAllDesignAbstractions("MODIFIES", "ASSIGNMENT");
 
-        pkbTest.clearAllDatabases();
-
-        Result pkbResultAfterClearing = pkbTest.getDesignAbstraction("MODIFIES", make_pair("ASSIGNMENT", "ma9"));
-
-        REQUIRE(((pkbResultBeforeClearing.toString() == "MODIFIES:ASSIGNMENT: ma9: ma10, ma11, ma12, ")
-                 && (pkbResultAfterClearing.toString() == "none: none: none, ")));
+        REQUIRE(result.size() != 0);
     }
 }
 
-TEST_CASE("Test 9: Retrieval of All AssignmentModifies Design Abstractions") {
+TEST_CASE("Test 8: Retrieval of Variables Captured By AssignmentModifies Design Abstractions") {
     SECTION("") {
-        PKB pkbTest = PKB();
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma13", "ma14"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma15", "ma16"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma17", "ma18"));
+        DesignAbstractionsController designAbstractionsControllerTest = DesignAbstractionsController();
 
-        vector<Result> result = pkbTest.getAllDesignAbstractions("MODIFIES", "ASSIGNMENT");
-
-        REQUIRE(result.size() == 3);
-    }
-}
-
-TEST_CASE("Test 10: Retrieval of Variables Captured By AssignmentModifies Design Abstractions") {
-    SECTION("") {
-        PKB pkbTest = PKB();
-
-        pkbTest.clearAllDatabases();
-
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma19", "ma20"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma21", "ma22"));
-        pkbTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma23", "ma24"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma19", "ma20"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma21", "ma22"));
+        designAbstractionsControllerTest.addDesignAbstraction("MODIFIES", make_tuple("ASSIGNMENT", "ma23", "ma24"));
 
         unordered_map<string, unordered_set<string>> result =
-                pkbTest.getAllVariablesCapturedByDesignAbstraction("MODIFIES", "ASSIGNMENT");
+                designAbstractionsControllerTest.getAllVariablesCapturedByDesignAbstraction("MODIFIES", "ASSIGNMENT");
 
-        REQUIRE(result.size() == 3);
+        REQUIRE(
+                ((result.find("ma20") != result.end()) &&
+                 (result.find("ma22") != result.end()) &&
+                 (result.find("ma24") != result.end()))
+        );
     }
 }
