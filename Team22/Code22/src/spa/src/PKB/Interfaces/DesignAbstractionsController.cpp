66987a5cbe90de3269c396cc95ad478002fb3caf
Jai Lulla <jaylulla9@gmail.com>
2023-03-30 22:43:28 +0800

    Add DesignAbstractionsController

---
+++ Team22/Code22/src/spa/src/PKB/Interfaces/DesignAbstractionsController.cpp
@@ -0,0 +1,74 @@
+//
+// Created by Jai Lulla  on 30/3/23.
+//
+
+#include <iostream>
+
+#include "DesignAbstractionsController.h"
+
+#include "PKB/DesignAbstractions/DesignAbstractionsFactory.h"
+#include "PKB/DesignAbstractions/DesignAbstractionsDatabase/DesignAbstractionDatabase.h"
+#include "PKB/DesignAbstractions/DesignAbstractionsDatabase/DesignAbstractionsDatabaseFactory.h"
+
+#include "PKB/Exceptions/InvalidAPICallException.cpp"
+
+using namespace std;
+
+/**
+ * This method allows to add a Design Abstraction to the Program Knowledge Base.
+ *
+ * @param designAbstraction The type of Abstraction that is to be added.
+ * @param abstractionDetails A tuple, which takes in 3 strings. which contains the details about the Abstraction.
+ */
+void DesignAbstractionsController::addDesignAbstraction(string designAbstraction, tuple<string, string, string> abstractionDetails) {
+    DesignAbstraction* da = DesignAbstractionsFactory::createDesignAbstraction(designAbstraction, abstractionDetails);
+    da->addToDatabase();
+}
+
+void DesignAbstractionsController::addDesignAbstraction(string designAbstraction, tuple<string, string> abstractionDetails) {
+    if (designAbstraction == "USES" || designAbstraction == "MODIFIES") {
+        throw InvalidAPICallException((designAbstraction + " Cannot Be Accessed Via This API").data());
+    }
+
+    this->addDesignAbstraction(designAbstraction,
+                               make_tuple("_", get<0>(abstractionDetails), get<1>(abstractionDetails)));
+}
+
+/**
+ * Returns a Result Object, containing the Design Abstraction Details. These can
+ * be accessed by the Result methods to be acted upon further.
+ *
+ * @param abstractionType Type of Abstraction to obtain.
+ * @param query Query contains the Design Entity type being abstracted along with the Entity Name.
+ * @return Result object with the result or "None" if the result does not exist.
+ */
+Result DesignAbstractionsController::getDesignAbstraction(string abstractionType, tuple<string, string> query) {
+    DesignAbstractionDatabase* db = DesignAbstractionsDatabaseFactory::getAbstractionDatabase(abstractionType,
+                                                                                              get<0>(query));
+    Result queryResult = db->getFromDatabase(get<1>(query));
+
+    return queryResult;
+}
+
+Result DesignAbstractionsController::getDesignAbstraction(string abstractionType, string query) {
+    if (abstractionType == "USES" || abstractionType == "MODIFIES") {
+        throw InvalidAPICallException((abstractionType + " Cannot Be Accessed Via This API").data());
+    }
+
+    return this->getDesignAbstraction(abstractionType, make_tuple("_", query));
+}
+
+vector<Result> DesignAbstractionsController::getAllDesignAbstractions(string designAbstractionType, string entityTypeBeingAbstracted) {
+    DesignAbstractionDatabase* db =
+            DesignAbstractionsDatabaseFactory::getAbstractionDatabase(designAbstractionType,
+                                                                      entityTypeBeingAbstracted);
+    return db->getAllFromDatabase();
+}
+
+unordered_map<string, unordered_set<string>>
+DesignAbstractionsController::getAllVariablesCapturedByDesignAbstraction(string designAbstractionType, string entityTypeBeingAbstracted) {
+    DesignAbstractionDatabase* db =
+            DesignAbstractionsDatabaseFactory::getAbstractionDatabase(designAbstractionType,
+                                                                      entityTypeBeingAbstracted);
+    return db->getAllVariablesCaptured();
+}
---
+++ Team22/Code22/src/spa/src/PKB/Interfaces/DesignAbstractionsController.h
@@ -0,0 +1,29 @@
+//
+// Created by Jai Lulla  on 30/3/23.
+//
+
+#pragma once
+
+#ifndef SPA_DESIGNABSTRACTIONSCONTROLLER_H
+#define SPA_DESIGNABSTRACTIONSCONTROLLER_H
+
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "Result.h"
+
+using namespace std;
+
+class DesignAbstractionsController {
+    void addDesignAbstraction(string designAbstraction, tuple<string, string, string> abstractionDetails);
+    Result getDesignAbstraction(string abstractionType, tuple<string, string> query);
+    // Overloaded APIs, for all except modifies and uses
+    void addDesignAbstraction(string designAbstraction, tuple<string, string> abstractionDetails);
+    Result getDesignAbstraction(string abstractionType, string query);
+    vector<Result> getAllDesignAbstractions(string designAbstractionType, string entityTypeBeingAbstracted);
+    unordered_map<string, unordered_set<string>> getAllVariablesCapturedByDesignAbstraction(
+            string designAbstractionType, string entityTypeBeingAbstracted);
+};
+
+#endif //SPA_DESIGNABSTRACTIONSCONTROLLER_H
