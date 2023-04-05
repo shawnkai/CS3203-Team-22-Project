482247ec11023bf3c37f2a7795f9d9cbba9f3739
Atin Sakkeer Hussain <atinsaki@gmail.com>
2023-04-05 12:50:53 +0800

    Add Evaluate for Next*

--- Team22/Code22/src/spa/src/QPS/Expressions/NextExpression/NextExpression.cpp
+++ Team22/Code22/src/spa/src/QPS/Expressions/NextExpression/NextExpression.cpp
@@ -61,8 +61,121 @@ ResultTable* NextExpression::evaluate(PKB pkb) {
 
 }
 
+void NextStarExpression::traversal(int current, map<int, vector<int>> &graph, vector<string> &first, vector<string> &end,
+                              map<int, set<int>> &results, map<int, vector<int>> &stmtsInBlock, map<int, int> &seen, vector<string> prevs) {
+    //end of graph
+    if (current == 0) {
+        return;
+    }
+
+    //process any statements at most twice
+    if (seen.find(current) != seen.end()) {
+        if (seen.find(current)->second == 2) {
+            return;
+        } else {
+            seen.find(current)->second += 1;
+        }
+    } else {
+        seen.insert({current, 1});
+    }
+
+    for (int stmt : stmtsInBlock.find(current)->second) {
+        if (Utilities::checkIfPresent(end, to_string(stmt))) {
+            for (const auto& kv : results) {
+                if (Utilities::checkIfPresent(prevs, to_string(kv.first))) {
+                    results[kv.first].insert(stmt);
+                }
+            }
+        }
+        if (Utilities::checkIfPresent(first, to_string(stmt)) && results.find(stmt) == results.end()) {
+            results.insert({stmt, {}});
+        }
+        prevs.push_back(to_string(stmt));
+    }
+
+    for (int block : graph.find(current)->second) {
+        traversal(block, graph, first, end, results, stmtsInBlock, seen, prevs);
+    }
+}
+
 ResultTable* NextStarExpression::evaluate(PKB pkb) {
-    return new BooleanFalseTable();
+    for (string entity : {"ASSIGNMENT", "WHILE", "IF", "CALL", "READ", "PRINT"}) {
+        vector<Result> entityRes = pkb.getAllDesignEntity(entity);
+        for (Result res : entityRes) {
+            for (const string& line : res.getQueryResult()) {
+                this->stmtTypes.insert({stoi(line), entity});
+            }
+        }
+    }
+
+    vector<string> firstLine;
+    vector<string> secondLine;
+
+    if (dynamic_cast<StmtEntity*>(this->entities[0])) {
+        firstLine.push_back(to_string(dynamic_cast<StmtEntity*>(this->entities[0])->getLine()));
+    } else {
+        auto vars1 = pkb.getAllDesignEntity(this->entities[0]->getType());
+        for (Result res : vars1) {
+            for (const string& line : res.getQueryResult()) {
+                firstLine.push_back(line);
+            }
+        }
+    }
+
+    if (dynamic_cast<StmtEntity*>(this->entities[1])) {
+        secondLine.push_back(to_string(dynamic_cast<StmtEntity*>(this->entities[1])->getLine()));
+    } else {
+        auto vars2 = pkb.getAllDesignEntity(this->entities[1]->getType());
+        for (Result res : vars2) {
+            for (const string& line : res.getQueryResult()) {
+                secondLine.push_back(line);
+            }
+        }
+    }
+
+    vector<Result> procResults = pkb.getAllDesignEntity("PROCEDURE");
+    vector<string> procs;
+    for (auto res : procResults) {
+        procs.push_back(res.getQueryEntityName());
+    }
+
+    map<string, vector<string>> finalResult = {{this->entities[0]->toString(), {}}, {this->entities[1]->toString(), {}}};
+
+    for (const string& proc : procs) {
+        auto graph = pkb.getBlockToBlockDatabase(proc);
+        auto blockStatements = pkb.getBlockToStatementNumbersDatabase(proc);
+        vector<int> blocks;
+        for (const auto& kv : graph) {
+            blocks.push_back(kv.first);
+            vector<int> stmts = blockStatements.at(kv.first);
+        }
+        int start = *min_element(blocks.begin(), blocks.end());
+        map<int, set<int>> results = {};
+        map<int, int> seen;
+        traversal(start, graph, firstLine, secondLine, results, blockStatements, seen, {});
+        for (const auto& kv : results) {
+            for (int v : kv.second) {
+                finalResult.at(this->entities[0]->toString()).push_back(to_string(kv.first));
+                finalResult.at(this->entities[1]->toString()).push_back(to_string(v));
+            }
+        }
+    }
+
+    if (!dynamic_cast<SynonymStmtEntity*>(this->entities[0]) && !dynamic_cast<SynonymStmtEntity*>(this->entities[1])) {
+        if ((new ResultTable(finalResult))->getSize() != 0) {
+            return new BooleanTrueTable();
+        }
+        else {
+            return new BooleanFalseTable();
+        }
+    }
+    else if (!dynamic_cast<SynonymStmtEntity*>(this->entities[0])) {
+        return (new ResultTable(finalResult))->getColumns({this->entities[1]->toString()});
+    }
+    else if (!dynamic_cast<SynonymStmtEntity*>(this->entities[1])) {
+        return (new ResultTable(finalResult))->getColumns({this->entities[0]->toString()});
+    }
+    return new ResultTable(finalResult);
 }
 
 NextExpression::NextExpression(StmtRef* s1, StmtRef* s2) : Expression({s1, s2}) {}
--- Team22/Code22/src/spa/src/QPS/Expressions/NextExpression/NextExpression.h
+++ Team22/Code22/src/spa/src/QPS/Expressions/NextExpression/NextExpression.h
@@ -3,6 +3,7 @@
 #define SPA_NEXTEXPRESSION_H
 
 #include "QPS/Expressions/Expressions.h"
+#include <set>
 
 class NextExpression : public Expression {
 
@@ -19,7 +20,14 @@ public:
 };
 
 class NextStarExpression : public Expression {
+private:
+    map<int, string> stmtTypes;
+
+    void traversal(int current, map<int, vector<int>> &graph, vector<string> &first, vector<string> &end,
+                   map<int, set<int>> &results, map<int, vector<int>> &stmtsInBlock, map<int, int> &seen, vector<string> prevs);
+
 public:
+
     explicit NextStarExpression(StmtRef* s1, StmtRef* s2);
 
     string toString() override;
