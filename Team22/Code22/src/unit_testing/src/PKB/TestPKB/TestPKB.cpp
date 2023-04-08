28c8c6145f4e9d10454e552c1b1a8b5f1930be39
Jai Lulla <jaylulla9@gmail.com>
2023-04-08 09:50:56 +0800

    Add More Unit Testing For PKB: clearAllDatabases() API

--- Team22/Code22/src/unit_testing/src/PKB/TestPKB/TestPKB.cpp
+++ Team22/Code22/src/unit_testing/src/PKB/TestPKB/TestPKB.cpp
@@ -17,7 +17,7 @@ TEST_CASE("Test 1: Test clearAllDatabases() API With Correct Key") {
         PKB pkb = PKB();
         pkb.addDesignEntity("PROCEDURE", make_tuple("One", "1"));
 
-        Result expectedResult = Result("PROCEDURE", "One", vector<string> {"1"});
+        Result expectedResult = Result("PROCEDURE", "One", vector<string>{"1"});
         Result result = pkb.getDesignEntity("PROCEDURE", "One");
 
         REQUIRE(expectedResult.areEqual(result));
@@ -33,7 +33,7 @@ TEST_CASE("Test 1: Test clearAllDatabases() API With Correct Key") {
         PKB pkb = PKB();
         pkb.addDesignAbstraction("FOLLOWS", make_tuple("1", "2"));
 
-        Result expectedResult = Result("FOLLOWS", "1", vector<string> {"2"});
+        Result expectedResult = Result("FOLLOWS", "1", vector<string>{"2"});
         Result result = pkb.getDesignAbstraction("FOLLOWS", "1");
 
         REQUIRE(expectedResult.areEqual(result));
@@ -101,11 +101,141 @@ TEST_CASE("Test 1: Test clearAllDatabases() API With Correct Key") {
 
         PKB pkb = PKB();
         pkb.addControlFlowGraph("Test",
-                                vector<int> {1},
-                                unordered_map<int, vector<int>> {make_pair(1, vector<int> {1})},
-                                unordered_map<int, int> {make_pair(1, 1)},
-                                unordered_map<int, vector<int>> {make_pair(1, vector<int> {1})},
-                                unordered_set<int> {1});
+                                vector<int>{1},
+                                unordered_map<int, vector<int>>{make_pair(1, vector<int>{1})},
+                                unordered_map<int, int>{make_pair(1, 1)},
+                                unordered_map<int, vector<int>>{make_pair(1, vector<int>{1})},
+                                unordered_set<int>{1});
+
+        vector<int> topologicallySortedElements = pkb.getTopologicallySortedElementsDatabase("Test");
+        unordered_map<int, vector<int>> blockToStatementNumberElements = pkb.getBlockToStatementNumbersDatabase("Test");
+        unordered_map<int, int> statementNumberToBlockElements = pkb.getStatementNumberToBlockDatabase("Test");
+        unordered_map<int, vector<int>> blockToBlockElements = pkb.getBlockToBlockDatabase("Test");
+        unordered_set<int> blocksWithBackPointersElements = pkb.getBlocksWithBackPointersDatabase("Test");
+
+        REQUIRE(topologicallySortedElements.size() == 1);
+        REQUIRE(blockToStatementNumberElements.size() == 1);
+        REQUIRE(statementNumberToBlockElements.size() == 1);
+        REQUIRE(blockToBlockElements.size() == 1);
+        REQUIRE(blocksWithBackPointersElements.size() == 1);
+
+        pkb.clearAllDatabases(accessKey);
+
+        vector<int> topologicallySortedElementsAfterClearing =
+                pkb.getTopologicallySortedElementsDatabase("Test");
+        unordered_map<int, vector<int>> blockToStatementNumberElementsAfterClearing =
+                pkb.getBlockToStatementNumbersDatabase("Test");
+        unordered_map<int, int> statementNumberToBlockElementsAfterClearing =
+                pkb.getStatementNumberToBlockDatabase("Test");
+        unordered_map<int, vector<int>> blockToBlockElementsAfterClearing =
+                pkb.getBlockToBlockDatabase("Test");
+        unordered_set<int> blocksWithBackPointersElementsAfterClearing =
+                pkb.getBlocksWithBackPointersDatabase("Test");
+
+        REQUIRE(topologicallySortedElementsAfterClearing.empty());
+        REQUIRE(blockToStatementNumberElementsAfterClearing.empty());
+        REQUIRE(statementNumberToBlockElementsAfterClearing.empty());
+        REQUIRE(blockToBlockElementsAfterClearing.empty());
+        REQUIRE(blocksWithBackPointersElementsAfterClearing.empty());
+    }
+}
+
+TEST_CASE("Test 2: Test clearAllDatabases() API With Incorrect Key") {
+    SECTION("Testing Design Entities Database") {
+        string accessKey = "InvalidKey";
+
+        PKB pkb = PKB();
+        pkb.addDesignEntity("PROCEDURE", make_tuple("One", "1"));
+
+        Result expectedResult = Result("PROCEDURE", "One", vector<string>{"1"});
+        Result result = pkb.getDesignEntity("PROCEDURE", "One");
+
+        REQUIRE(expectedResult.areEqual(result));
+
+        pkb.clearAllDatabases(accessKey);
+
+        REQUIRE(pkb.getNumberOfDesignEntity("PROCEDURE") != 0);
+    }
+
+    SECTION("Testing Design Abstractions Database") {
+        string accessKey = "InvalidKey";
+
+        PKB pkb = PKB();
+        pkb.addDesignAbstraction("FOLLOWS", make_tuple("1", "2"));
+
+        Result expectedResult = Result("FOLLOWS", "1", vector<string>{"2"});
+        Result result = pkb.getDesignAbstraction("FOLLOWS", "1");
+
+        REQUIRE(expectedResult.areEqual(result));
+
+        pkb.clearAllDatabases(accessKey);
+
+        Result resultAfterClearing = pkb.getDesignAbstraction("FOLLOWS", "1");
+
+        REQUIRE(resultAfterClearing.getQueryEntityName() != "none");
+    }
+
+    SECTION("Testing Assign Pattern Database") {
+        string accessKey = "InvalidKey";
+
+        PKB pkb = PKB();
+        pkb.addAssignPattern("a", "a + b", "1");
+
+        string result = pkb.getRightHandExpressionOfAVariableOnAParticularLineNumber("a", "1");
+
+        REQUIRE(result.compare("a + b") == 0);
+
+        pkb.clearAllDatabases(accessKey);
+
+        string resultAfterClearing = pkb.getRightHandExpressionOfAVariableOnAParticularLineNumber("a", "1");
+
+        REQUIRE(!resultAfterClearing.empty());
+    }
+
+    SECTION("Testing If Pattern Database") {
+        string accessKey = "InvalidKey";
+
+        PKB pkb = PKB();
+        pkb.addPattern("IF", "1", "a");
+
+        unordered_set<string> result = pkb.getAllVariablesUsedInPattern("IF", "1");
+
+        REQUIRE((result.find("a") != result.end()));
+
+        pkb.clearAllDatabases(accessKey);
+
+        unordered_set<string> resultAfterClearing = pkb.getAllVariablesUsedInPattern("IF", "1");
+
+        REQUIRE(!resultAfterClearing.empty());
+    }
+
+    SECTION("Testing While Pattern Database") {
+        string accessKey = "InvalidKey";
+
+        PKB pkb = PKB();
+        pkb.addPattern("WHILE", "1", "a");
+
+        unordered_set<string> result = pkb.getAllVariablesUsedInPattern("WHILE", "1");
+
+        REQUIRE((result.find("a") != result.end()));
+
+        pkb.clearAllDatabases(accessKey);
+
+        unordered_set<string> resultAfterClearing = pkb.getAllVariablesUsedInPattern("WHILE", "1");
+
+        REQUIRE(!resultAfterClearing.empty());
+    }
+
+    SECTION("Testing Control Flow Graph Database") {
+        string accessKey = "InvalidKey";
+
+        PKB pkb = PKB();
+        pkb.addControlFlowGraph("Test",
+                                vector<int>{1},
+                                unordered_map<int, vector<int>>{make_pair(1, vector<int>{1})},
+                                unordered_map<int, int>{make_pair(1, 1)},
+                                unordered_map<int, vector<int>>{make_pair(1, vector<int>{1})},
+                                unordered_set<int>{1});
 
         vector<int> topologicallySortedElements = pkb.getTopologicallySortedElementsDatabase("Test");
         unordered_map<int, vector<int>> blockToStatementNumberElements = pkb.getBlockToStatementNumbersDatabase("Test");
